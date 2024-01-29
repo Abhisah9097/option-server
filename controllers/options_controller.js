d5a832ab3902d8effa84e0ddb3cd0bce1e49c384
Abhishek Kumar <aksing9097@gmail.com>
2024-01-30 01:20:53 +0530

    update new codes

--- controllers/options_controller.js
+++ controllers/options_controller.js
@@ -1,39 +1,65 @@
-require('dotenv').config();
 const axios = require('axios');
-// Function to make API call to NSE with headers and params
-async function callNseApi(req, res, next) {
-  const nseApiEndpoint = process.env.API_URL || "";
-  // const userAgent = req.headers['user-agent'];
-  // Define headers and params
-  const headers = {
-    "User-Agent": req.headers['user-agent'] || "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept": req.headers['accept'] || "application/json",
-    "Accept-Encoding": req.headers['accept-encoding'] || "gzip, deflate, br",
-    "Accept-Language": req.headers['accept-language'] || "en-US,en;q=0.9,hi;q=0.8",
-    "Cookie": req.headers['cookie'] || "",
-    "Content-Type": req.headers['content-type'] || "application/json",
-    "Authorization": req.headers['authorization'] || "",
-    "Cache-Control": req.headers['cache-control'] || "no-cache",
-    "Connection": req.headers['connection'] || "keep-alive",
-    "DNT": req.headers['dnt'] || "1",
-    "Referer": req.headers['referer'] || ""
-  };
-  
 
-  console.log("URL: ", headers, nseApiEndpoint);
-  const params = {
-    symbol: "NIFTY"
-  };
+// Initialize cookie variable and headers
+let cookie;
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+  'Accept': 'application/json',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Accept-Language': 'en-US,en;q=0.9,hi;q=0.8',
+  'Cookie': cookie || ''
+};
+
+// Create Axios instance with base URL and headers
+const instance = axios.create({
+  headers: headers,
+  timeout: 5000, // Set timeout as needed
+});
+
+const getCookies = async () => {
   try {
-    const response = await axios.get(nseApiEndpoint, {
-      headers: headers,
-      params: params,
-    });
-    console.log(response.data);
-    res.json(response.data);
+    const response = await instance.get(process.env.API_URL || "");
+    cookie = response.headers['set-cookie'].join();
   } catch (error) {
-    res.json(error);
+    console.error("getCookies =========> error");
   }
 }
+
+const callNseApi = async (req, res, next) => {
+  let retries = 3;
+
+  while (retries > 0) {
+    try {
+      // Set the Cookie header before making the request
+      headers['Cookie'] = cookie || '';
+      headers['User-Agent'] = req.headers['user-agent'] || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)';
+      console.log("Headers: ", headers);
+      const response = await instance.get(process.env.API_URL || "", {
+        params: {
+          symbol: "NIFTY"
+        },
+      });
+
+      console.log("Success: ", 200);
+      res.json(response.data);
+      return; // Exit the function if successful
+    } catch (error) {
+      console.error("Error: ", error.message);
+
+      if (error.response && error.response.status === 401) {
+        console.log("callNseApi =========> error.status === 401");
+        if (!cookie) {
+          console.log("callNseApi =========> cookie not found");
+          await getCookies();
+        }
+      }
+
+      retries--;
+    }
+  }
+
+  // If retries are exhausted
+  res.status(500).json({ error: "Failed to fetch data from NSE API" });
+};
 
 module.exports = callNseApi;
