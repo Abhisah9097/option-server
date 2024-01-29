c14857bd36777685b007d2aff3449c232959efb2
Abhishek Kumar <aksing9097@gmail.com>
2024-01-30 00:40:18 +0530

    updarte - user-agent

--- controllers/options_controller.js
+++ controllers/options_controller.js
@@ -6,13 +6,14 @@ async function callNseApi(req, res, next) {
   const userAgent = req.headers['user-agent'];
   // Define headers and params
   const headers = {
-    "User-Agent": userAgent,
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
     "Accept": "application/json",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9,hi;q=0.8",
     "Cookie": ""
   };
 
+  console.log("URL: ", nseApiEndpoint);
   const params = {
     symbol: "NIFTY"
   };
@@ -21,6 +22,7 @@ async function callNseApi(req, res, next) {
       headers: headers,
       params: params,
     });
+    console.log(response.data);
     res.json(response.data);
   } catch (error) {
     res.json(error);
